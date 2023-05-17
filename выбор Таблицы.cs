51628fe23ae4f123cbec29c787ba4794273582f5
DriveFallen <kobtsev2003@mail.ru>
2023-05-17 03:42:58 +0300

    first commit

---
+++ выбор Таблицы.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_5
+{
+    public partial class выбор_Таблицы : Form
+    {
+        public выбор_Таблицы()
+        {
+            InitializeComponent();
+        }
+    }
+}
